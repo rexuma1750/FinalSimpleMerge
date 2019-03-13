6abc423751c5452ec47c9489994b4afc344b9653
runnie <rexuma1750@bths.edu>
2019-03-12 22:57:24 -0400

    final version

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -6,7 +6,36 @@
  */
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
+        int a=0, b=0, n=0;
 
+        int[]arr=new int[arr1.length+arr2.length];
+        while(a<arr1.length&&arr2.length){
+            if(arr1[a]<arr2[b]){
+                arr[n]=arr1[a];
+                n++;
+                a++;
+            }
+            else{
+                arr[n]=arr2[b];
+                n++;
+                b++;
+            }
+        }
+if(a>b){
+    while(b<arr2.length){
+        arr[n]=arr2[b];
+        n++;
+        b++;
+    }
+    else{
+        while(a<arr1.length){
+            arr[n]=arr1[a];
+            n++;
+            a++;
+        }
+    }
+    return arr;
+}
     }
 
 }
